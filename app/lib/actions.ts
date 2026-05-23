213c527a3b4396656b6236675c66ee0bc6a50aa5
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2026-05-23 18:28:17 -0300

    Fix build

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -113,20 +113,17 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-    // throw new Error('Failed to Delete Invoice');
-    try {
-        await sql`
-            DELETE FROM invoices
-            WHERE id = ${id}
-        `;
-    } catch (error) {
-        console.error(error);
-        return {
-            message: 'Database Error: Failed to Delete Invoice.',
-        };
-    }
+  // throw new Error('Failed to Delete Invoice');
+  try {
+      await sql`
+          DELETE FROM invoices
+          WHERE id = ${id}
+      `;
+  } catch (error) { 
+    console.error('Database Error: Failed to Delete Invoice.', error); 
+  }
 
-    revalidatePath('/dashboard/invoices');
+  revalidatePath('/dashboard/invoices');
 }
 
 export async function authenticate(
